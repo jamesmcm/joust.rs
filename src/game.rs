e7bcc58a48bbb61dac19d5d45fe9084648971647
James McMurray <jamesmcm03@gmail.com>
2020-03-02 22:37:16 +0100

    TODO: Smoother jumping and deceleration, platforms, collisions

--- src/game.rs
+++ src/game.rs
@@ -3,6 +3,7 @@ const JOUSTER_HEIGHT: usize = 20;
 const JOUSTER_WIDTH: usize = 30;
 const JOUSTER_ACCELERATION_HORIZONTAL: f64 = 0.1;
 const MAX_HORIZONTAL_VELOCITY: f64 = 4.0;
+const MAX_VERTICAL_VELOCITY: f64 = 3.0;
 
 const ARENA_WIDTH: usize = 800;
 use super::input::*;
@@ -10,6 +11,7 @@ use super::input::*;
 pub struct Game {
     pub player: Player,
     pub other_jousters: Vec<Jouster>,
+    space_toggle: bool,
 }
 
 enum GameState {
@@ -22,6 +24,7 @@ impl Game {
         Game {
             player: Player::new(Position::from((400.0, 0.0))), //TODO: Handle dynamic canvas
             other_jousters: Vec::new(),
+            space_toggle: false,
         }
     }
 
@@ -33,6 +36,17 @@ impl Game {
             (false, true) => JOUSTER_ACCELERATION_HORIZONTAL,
             (true, true) => 0.0,
         };
+
+        if space_pressed() {
+            if self.space_toggle {
+                // Space is held down, no state change
+            } else {
+                self.space_toggle = true;
+                self.player.jouster.jump();
+            }
+        } else {
+            self.space_toggle = false;
+        }
 
         // Calculations
         self.player.jouster.update();
@@ -63,15 +77,18 @@ pub struct Jouster {
     mounted: bool,
     pub width: usize,
     pub height: usize,
+    pub state: JousterState,
 }
 
-enum JousterState {
+#[derive(PartialEq)]
+pub enum JousterState {
     Flying,
     Walking,
     Downed,
     WalkingDismounted,
 }
 
+use JousterState::*;
 impl Jouster {
     fn new(position: Position) -> Jouster {
         Jouster {
@@ -81,6 +98,7 @@ impl Jouster {
             mounted: true,
             width: JOUSTER_WIDTH,
             height: JOUSTER_HEIGHT,
+            state: Walking,
         }
     }
 
@@ -92,16 +110,41 @@ impl Jouster {
         } else if self.velocity.x < -MAX_HORIZONTAL_VELOCITY {
             self.velocity.x = -MAX_HORIZONTAL_VELOCITY;
         }
+
+        if self.velocity.y > MAX_VERTICAL_VELOCITY {
+            self.velocity.y = MAX_VERTICAL_VELOCITY;
+        } else if self.velocity.y < -MAX_VERTICAL_VELOCITY {
+            self.velocity.y = -MAX_VERTICAL_VELOCITY;
+        }
+
         self.position += self.velocity;
 
         if self.position.x > ARENA_WIDTH as f64 {
             self.position.x = 0.0;
-        } else if self.position.x < 0 as f64 {
+        } else if self.position.x < 0.0 {
             self.position.x = ARENA_WIDTH as f64;
         }
 
-        // TODO: Timer when downed/unmounted etc.
+        if self.position.y < 0.0 {
+            // TODO: Handle any platforms
+            self.position.y = 0.0;
+            self.velocity.y = 0.0;
+            self.acceleration.y = 0.0;
+            if self.state == Flying {
+                self.state = Walking;
+            }
+        }
+
+        if self.state == Flying {
+            self.acceleration.y -= 0.3;
+        }
     }
+
+    fn jump(&mut self) -> () {
+        self.acceleration.y += 14.0;
+        self.state = Flying;
+    }
+    // TODO: Timer when downed/unmounted etc.
 }
 
 struct Platform {}
